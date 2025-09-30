a7f094cb5f29cdb49b79a1334a45e9ad538a8d8d
BOUCHRIKA Yousri <BOUCHRIKA@lan.sio.lyceefulbert.fr>
2025-09-30 08:54:13 +0200

    dsfr + debut site

--- importmap.php
+++ importmap.php
@@ -25,4 +25,7 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    '@gouvfr/dsfr' => [
+        'path' => './node_modules/@gouvfr/dsfr/dist/dsfr.module.js',
+    ],
 ];
